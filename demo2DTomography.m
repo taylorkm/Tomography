19429450b0f5f78393a1b10a2d947fd7daf9397b
taylorkm <taylor.kye@gmail.com>
2014-02-27 21:54:43 -0500

    Orthoprojectors are so cool

---
+++ demo2DTomography.m
@@ -0,0 +1,77 @@
+% Create the square image, and get arrays ii and jj that have the relative
+% coordinates.
+nperdim = 128;
+xrng = linspace(-2, 2, nperdim);
+yrng = linspace(-2, 2, nperdim);
+[x1,x2] = meshgrid(xrng, yrng);
+x = [x1(:), x2(:)];
+f = max(abs(x), [], 2) <= 1;
+f = phantom(nperdim);
+
+% coordinates of centers in x1x2-plane
+[ii,jj] = meshgrid(1:nperdim, 1:nperdim);
+ii = ii-ii(1, ceil(nperdim/2));
+jj = jj-jj(ceil(nperdim/2), 1);
+coords = [ii(:),jj(:)];
+rads = sqrt(sum(coords.^2,2)); % radius of each coord from origin
+maxrad = max(rads);
+
+nTheta = 100;
+thetas = pi*(0:nTheta-1)/nTheta;
+nBins = ceil(1.45*nperdim);
+
+bnds = - (ceil(maxrad)+1):2*(ceil(maxrad)+1)/nBins:(ceil(maxrad)+1);
+
+R = zeros(nBins, nTheta);
+for i = 1:nTheta
+    th = thetas(i);
+    % Build projector onto subspace orthogonal to direction of scans.
+    u = [cos(th);sin(th)];     
+    P = u*u'; % orthoprojector onto subspace spanned by u
+    Q = [cos(th),-sin(th);sin(th),cos(th)];    
+    PQ = P'*Q;       
+    newCoords = coords*PQ;
+    
+    x = newCoords(:,1); % the only coordinates that matter
+    [xsrtd,idx] = sort(x);
+    
+    % A crude quadrature
+    j = 1;
+    k = 1;
+    while j <= nBins && k <= nperdim^2
+        if ( xsrtd(k) < bnds(j+1) )
+            R(j,i) = R(j,i) + f(idx(k));
+            k = k+1;
+        else            
+            j = j+1;    
+        end
+    end
+end
+figure
+imagesc(R)
+colormap hot
+title('Radon Transform')
+
+
+
+dtheta = abs(thetas(2)-thetas(1));
+
+recon = zeros(nperdim, nperdim);
+
+map2idx = @(x) round((x-bnds(1))/(bnds(2)-bnds(1))) + 1;
+
+for t = 1:nTheta
+    temp = ii*cos(thetas(t))+jj*sin(thetas(t));
+    i0 = map2idx(temp);
+    i0 = i0(:);
+    recon = recon + reshape(R(i0,t),nperdim,nperdim);
+end
+
+
+
+figure
+imagesc(recon)
+colormap gray
+title('Inverse Radon Transform')
+
+
